ad7c8be00a27bda64657de1c80a7c590998c4af3
Denis Markov <keynsianets@gmail.com>
2018-07-19 17:30:10 +0300

    Ready task 1.1

--- Quotes/ViewController.swift
+++ Quotes/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var quoteText: UILabel!
+    @IBOutlet weak var authorImage: UIImageView!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    
+    let quotes = [
+        "If you do something and it turns out pretty good, then you should go do something else wonderful, not dwell on it for too long. Just figure out what's next.",
+        "No matter how small you start, start something that matters.",
+        "The credit belongs to those who are actually in the arena, who strive valiantly; who know the great enthusiasums, the great devotions, and spend themselves in a worthy cause; who at best know the triumph of hogh achivement; and who, at worst, if they fail, fail while daring greatly, so that their place shall never be with those cold and timid souls who know neither victory nor defeat."]
+    
+    let authorNames = [
+        "Steve Jobs", "Brendon Burchard", "Theodore Roosevelt"
+    ]
+    let imageNames = [
+        "stevejobs", "brendon_burchard", "theodore_roosevelt"
+    ]
+
+    @IBAction func inspireMeDidTap(_ sender: Any) {
+        
+        let randonNumer = Int(arc4random()) % 3
+        
+        let newQuote = quotes[randonNumer]
+        let newAuthor = authorNames[randonNumer]
+        let authorImageName = imageNames[randonNumer]
+        quoteText.text = newQuote
+        authorImage.image = UIImage(named: authorImageName)
+        authorNameLabel.text = newAuthor
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
